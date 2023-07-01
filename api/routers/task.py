1457fc36f94d557fd4a596a80e1194f0f5c9c80c
ooooose <yuuki.oose1990@gmail.com>
2023-07-01 20:47:43 +0900

    delete実装

--- api/routers/task.py
+++ api/routers/task.py
@@ -31,5 +31,9 @@ async def update_task(task_id: int, task_body: task_schema.TaskCreate, db: Async
 
 
 @router.delete("/tasks/{task_id}", response_model=None)
-async def delete_task(task_id: int):
-    return
+async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
+    task = await task_handler.get_task(db, task_id = task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail='Task not found')
+
+    return await task_handler.delete_task(db, original = task)
