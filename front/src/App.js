227914d8c33433dd650224e6b18ce028d6463a72
ooooose <yuuki.oose1990@gmail.com>
2023-07-01 11:42:28 +0900

    APIから情報を取得

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+  const url = "http://127.0.0.1:8000";
+  const GetData = () => {
+    axios.get(url).then((res) => {
+      setData(res.data);
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>ここに処理を書いていきます</div>
+			{data ? <div>{data.Hello}</div> : <button onClick={GetData}>データを取得</button>}
+		</>
+	);
 }
 
 export default App;
